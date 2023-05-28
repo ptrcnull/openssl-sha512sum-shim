90380491885020fbcf35c05f2e9b8ff138948dda
ptrcnull <git@ptrcnull.me>
2023-05-28 15:28:43 +0200

    fix: allow for paths with spaces for benchmarking

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 hyperfine --warmup 1 --shell=none \
-    "./build/sha512sum $1" \
-    "/usr/bin/sha512sum $1" \
-    "/usr/bin/openssl dgst -sha512 -r $1"
+    "./build/sha512sum '$1'" \
+    "/usr/bin/sha512sum '$1'" \
+    "/usr/bin/openssl dgst -sha512 -r '$1'"
